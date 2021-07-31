b032d5ae7ccc5d70716d04c1185e61dcc1784901
Alexander Bulanov <develop@abulanov.com>
2021-08-01 02:43:14 +0300

    Created and styled main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
 import "./app.css";
+import pm from "./pm.jpg";
 
 function App() {
   return (
     <div className="App">
-      <div className="container">Hello!</div>
+      <div className="container">
+        <div className="navbar">
+          <div className="navbar__logo">FPM</div>
+          <div className="navbar__menu">
+            <a className="navbar__menuitem" href="#login">
+              Login
+            </a>
+            <div className="navbar__menusep">|</div>
+            <a className="navbar__menuitem" href="#register">
+              Register
+            </a>
+          </div>
+        </div>
+        <div className="main">
+          <div className="descphoto">
+            <img src={pm} width="400" height="500" />
+          </div>
+          <div className="desctext">
+            <p>Description text. Lorem ipsum.</p>
+          </div>
+        </div>
+        <hr className="footer_divider" />
+        <div className="footer">
+          <div className="copyright">&copy; 2021 Flexoprom LLC</div>
+        </div>
+      </div>
     </div>
   );
 }
