6b67f4c19cf8f8ba8f68572d1be71b6aadd35155
Alexander Bulanov <develop@abulanov.com>
2021-08-03 21:14:25 +0300

    Clean React Typescript install

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Flexoprom Project Management Suite
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
