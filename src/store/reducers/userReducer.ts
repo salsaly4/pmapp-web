3237cb26ac7db979e06727ce5aaa635a0168d095
Alexander Bulanov <develop@abulanov.com>
2021-08-04 10:09:16 +0300

    Created boilerplate for userReducer

--- src/store/reducers/userReducer.ts
+++ src/store/reducers/userReducer.ts
@@ -19,16 +19,88 @@ const initialState: IUserState = {
     emailNotifications: false,
   },
   connectedApps: [],
+  loading: false,
+  saved: false,
+  error: false,
 };
 
 export const userReducer = (state = initialState, action: UserAction): IUserState => {
   switch (action.type) {
-    case UserActionTypes.SET_USER_ID:
-      return { ...state, id: action.payload };
-    case UserActionTypes.SET_USER_NAME:
+    case UserActionTypes.FETCH_USER:
       return {
         ...state,
-        fullName: { firstName: action.payload.firstName, lastName: action.payload.lastName },
+        loading: true,
+        error: false,
+      };
+    case UserActionTypes.FETCH_USER_SUCCESS:
+      return {
+        ...action.payload,
+        loading: false,
+        error: false,
+        saved: true,
+      };
+    case UserActionTypes.FETCH_USER_ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: true,
+      };
+    case UserActionTypes.SAVE_USER:
+      return {
+        ...state,
+        loading: true,
+        saved: false,
+        error: false,
+      };
+    case UserActionTypes.SAVE_USER_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        saved: true,
+        error: false,
+      };
+    case UserActionTypes.SAVE_USER_ERROR:
+      return {
+        ...state,
+        loading: false,
+        saved: false,
+        error: true,
+      };
+    case UserActionTypes.CHANGE_NAME:
+      return {
+        ...state,
+        fullName: action.payload,
+        saved: false,
+      };
+    case UserActionTypes.CHANGE_AVATAR:
+      return {
+        ...state,
+        avatar: action.payload,
+        saved: false,
+      };
+    case UserActionTypes.GRANT_SUPPORT_ACCESS:
+      return {
+        ...state,
+        supportAccess: action.payload,
+        saved: false,
+      };
+    case UserActionTypes.SET_NOTIFICATIONS:
+      return {
+        ...state,
+        notifications: action.payload,
+        saved: false,
+      };
+    case UserActionTypes.CONNECT_APP:
+      return {
+        ...state,
+        connectedApps: [...state.connectedApps, action.payload],
+        saved: false,
+      };
+    case UserActionTypes.DISCONNECT_APP:
+      return {
+        ...state,
+        connectedApps: state.connectedApps.filter((app) => app.id !== action.payload),
+        saved: false,
       };
     default:
       return state;
